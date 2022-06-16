c7cdd9bd63359f2ef5eb0b473e24722934665622
Bryan-Ricardo <programacionprogramacion65@gmail.com>
2022-06-16 14:37:28 -0500

    v1

---
+++ src/main/java/com/proyectos/spring/app/TiendaSpringUnoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectos.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSpringUnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaSpringUnoApplication.class, args);
+    }
+
+}
